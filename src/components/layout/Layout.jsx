069c8f14f21f5ee7d6a695c5d5a243ef1622bc2b
Alok <alok48ax@gmail.com>
2025-04-19 06:55:53 +0530

    Update Layout.jsx

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,27 +1,13 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Navbar from '../navbar/Navbar'
 import Footer from '../footer/Footer'
-import  { useContext } from 'react'
-import myContext from '../../context/data/myContext'
-import Main from '../chatbot/Main'
-
 
 function Layout({ children }) {
-
-  const context = useContext(myContext)
-
-
-
   return (
     <div className="relative">
-      
-      <Main/>
-    
       <Navbar />
       <div className="content">
         {children}
-      </div>
-
       </div>
       <Footer />
     </div>
